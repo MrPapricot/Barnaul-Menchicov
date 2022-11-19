8273199711a3ce11eee49553aaaf7be19c784c56
dima <dmitriy.menchicov@gmail.com>
2022-11-19 10:12:32 +0700

    Create jahg.py

--- main1.py
+++ main1.py
@@ -5,19 +5,20 @@ from PyQt5.uic import loadUi
 from PyQt5.QtGui import QColor, QBrush, QPainter, QColorConstants
 from PyQt5.QtCore import QPoint
 from random import randint
+from Ui import Ui_MainWindow
 
 
-class Main_Window(QMainWindow):
+class Main_Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.button_pressed)
 
     def paintEvent(self, event) -> None:
         if self.do_paint:
             qp = QPainter(self)
-            qp.setBrush(QBrush(QColor(QColorConstants.Yellow)))
+            qp.setBrush(QBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255))))
             self.drawer(qp)
         self.do_paint = False
 
@@ -30,8 +31,7 @@ class Main_Window(QMainWindow):
         self.repaint()
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = Main_Window()
-    window.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+window = Main_Window()
+window.show()
+sys.exit(app.exec_())
